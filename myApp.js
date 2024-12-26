79e6a2ff40256e865829c2f7b81909626b67e3c6
Tsholofetso Pooe <pooetsholofetso@gmail.com>
2024-12-26 10:46:38 +0000

    removed mongoose.connect() options

--- myApp.js
+++ myApp.js
@@ -2,13 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Establish MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+mongoose.connect(process.env.MONGO_URI);
+  
 
 const { Schema } = mongoose;
 
